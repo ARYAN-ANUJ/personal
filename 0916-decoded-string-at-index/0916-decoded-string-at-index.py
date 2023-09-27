77be0b65e8a9d6b8995dd1c7ba6132b70f3acce3
ARYAN-ANUJ <113282142+ARYAN-ANUJ@users.noreply.github.com>
2023-09-28 00:22:19 +0530

    Time: 45 ms (10.1%), Space: 16.2 MB (86.87%) - LeetHub

---
+++ 0916-decoded-string-at-index/0916-decoded-string-at-index.py
@@ -0,0 +1,24 @@
+class Solution:
+    def decodeAtIndex(self, encodedString: str, k: int) -> str:
+        character_lengths = [0]  
+
+        for i in range(len(encodedString)):
+            if encodedString[i].isdigit():
+                length = character_lengths[-1] * int(encodedString[i])
+                character_lengths.append(length)
+            else:
+                length = character_lengths[-1] + 1
+                character_lengths.append(length)
+
+       
+        ln = len(character_lengths)
+        while character_lengths:
+            k %= character_lengths[-1]  
+            ln -= 1
+
+            if k == 0 and encodedString[ln - 1].isalpha():
+                return encodedString[ln - 1]
+
+            character_lengths.pop()
+
+        return ""
