32b196124707458b5277a80cc385b2b801f4e416
ARYAN-ANUJ <113282142+ARYAN-ANUJ@users.noreply.github.com>
2023-10-06 01:25:52 +0530

    Time: 112 ms (51.87%), Space: 17.6 MB (97.49%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -3,7 +3,7 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
         dic=defaultdict(int)
         l=len(nums)//3
-        ans=list()
+        ans=[]
         for i in nums:
             dic[i]+=1
         for k, v in dic.items():
