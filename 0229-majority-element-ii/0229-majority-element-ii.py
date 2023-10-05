0835628c2a6929409852c74b6c27304a93991f84
ARYAN-ANUJ <113282142+ARYAN-ANUJ@users.noreply.github.com>
2023-10-06 01:25:04 +0530

    Time: 107 ms (76.56%), Space: 17.8 MB (49.52%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -2,10 +2,11 @@ from collections import defaultdict
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
         dic=defaultdict(int)
+        l=len(nums)//3
         ans=list()
         for i in nums:
             dic[i]+=1
         for k, v in dic.items():
-            if v > len(nums)//3:
+            if v > l:
                 ans.append(k)
         return ans
