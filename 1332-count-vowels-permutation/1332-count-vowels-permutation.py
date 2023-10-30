77dd6a5d425c679f7198119f2c1fc5bb6aabeffc
ARYAN-ANUJ <113282142+ARYAN-ANUJ@users.noreply.github.com>
2023-10-30 23:43:44 +0530

    Time: 351 ms (35.92%), Space: 119.3 MB (17.72%) - LeetHub

---
+++ 1332-count-vowels-permutation/1332-count-vowels-permutation.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countVowelPermutation(self, n: int) -> int:
+        dp_array = [[0] * 5 for _ in range(n + 1)]
+        dp_array[1] = [1, 1, 1, 1, 1]
+        for i in range(2, n + 1):
+            dp_array[i][0] = dp_array[i - 1][1] + dp_array[i - 1][2] + dp_array[i - 1][4]
+            dp_array[i][1] = dp_array[i - 1][0] +  dp_array[i - 1][2]
+            dp_array[i][2] = dp_array[i - 1][1] + dp_array[i - 1][3]
+            dp_array[i][3] = dp_array[i - 1][2]
+            dp_array[i][4] = dp_array[i - 1][2] + dp_array[i - 1][3]
+        return sum(dp_array[n]) % ((10 ** 9) + 7)
